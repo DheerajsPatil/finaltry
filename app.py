b3a687988a91c283954dd7b0457f353014b5dc03
DheerajsPatil <100686851+DheerajsPatil@users.noreply.github.com>
2023-03-15 18:08:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from flask import Flask,request,jsonify
 import numpy as np
 import pickle
-import joblib
-from scipy.sparse import csr_matrix
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.datasets import fetch_openml
-from sklearn.utils import shuffle
 
 model = pickle.load(open('model.pkl','rb'))
 
